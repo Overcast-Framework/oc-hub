272360c35f6fbedb041403d98d8fd988037ec6c6
Wizard2D <namamg3@gmail.com>
2025-06-26 02:58:15 +0300

    initial commit

---
+++ Installer.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.Win32;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oc_hub
+{
+    public class Installer
+    {
+        public class ReleaseInfo
+        {
+            public string tag_name { get; set; }
+            public string html_url { get; set; }
+            public Asset[] assets { get; set; }
+        }
+
+        public class Asset
+        {
+            public string name { get; set; }
+            public string browser_download_url { get; set; }
+        }
+
+        public async Task<ReleaseInfo> GetLatestReleaseAsync()
+        {
+            using HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.UserAgent.ParseAdd("OvercastInstaller/1.0");
+
+            try
+            {
+                string url = "https://api.github.com/repos/Overcast-Framework/OvercastC/releases/latest";
+                var response = await client.GetStringAsync(url);
+
+                var release = JsonConvert.DeserializeObject<ReleaseInfo>(response);
+
+                return release;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+
+            throw new Exception("idk how we got here");
+        }
+
+        public async void Install()
+        {
+            if(Environment.OSVersion.Platform != PlatformID.Win32NT)
+            {
+                Console.WriteLine("oc-hub does not support non-Windows machines yet");
+                return;
+            }
+
+            // first: create all the directories
+            // mainly stuff resides in C:/Program Files/Overcast
+
+            Console.WriteLine("Creating Directories...");
+
+            try
+            {
+                var basePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Overcast");
+                Directory.CreateDirectory(basePath);
+                Directory.CreateDirectory(Path.Combine(basePath, "libraries")); // unused currently, but just future-proofing
+
+                // then just install the binary
+                Console.WriteLine("Downloading latest release...");
+
+                // query the latest release from GH
+                var releaseData = await GetLatestReleaseAsync();
+                Console.WriteLine("Latest release: " + releaseData.tag_name);
+
+                using WebClient wc = new WebClient();
+
+                wc.DownloadProgressChanged += (s, e) =>
+                {
+                    Console.SetCursorPosition(0, Console.CursorTop);
+                    Console.Write(e.ProgressPercentage + "% [" + (e.BytesReceived)/1000000 + " MB]");
+                };
+
+                wc.DownloadFileAsync(new Uri(releaseData.assets.FirstOrDefault(e => e.name == "overcastc-bin-win64.zip").browser_download_url), Path.Combine(basePath, "overcast.zip"));
+
+                while (wc.IsBusy)
+                    Thread.Sleep(100);
+
+                Console.WriteLine("\nDownloaded latest release");
+                Console.WriteLine("Unarchiving release zip...");
+
+                ZipFile.ExtractToDirectory(Path.Combine(basePath, "overcast.zip"), basePath);
+
+                Console.WriteLine("Cleaning up...");
+                File.Delete(Path.Combine(basePath, "overcast.zip"));
+
+                Console.WriteLine("Adding to PATH and env variables");
+
+                Environment.SetEnvironmentVariable("OvercastInstall", basePath, EnvironmentVariableTarget.User);
+                string path = Environment.GetEnvironmentVariable("Path", EnvironmentVariableTarget.User);
+                if (!path.Contains(basePath))
+                {
+                    path += ";" + basePath;
+                    Environment.SetEnvironmentVariable("Path", path, EnvironmentVariableTarget.User);
+                }
+
+                // create a version tracker
+                File.WriteAllText(Path.Combine(basePath, "VERSION"), releaseData.tag_name);
+                File.Move(Path.Combine(basePath, "Overcast.exe"), Path.Combine(basePath, "overcast.exe")); // for niceness
+
+                Console.WriteLine("Installation complete!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Installation Error: " + e.Message);
+                return;
+            }
+        }
+
+        public void Uninstall()
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿namespace oc_hub
+{
+    internal class Program
+    {
+        const string OC_HUB_VERSION = "v1.0.0";
+
+        static void Main(string[] args)
+        {
+            string helpText = @"oc-hub install | installs the latest version of OvercastC
+oc-hub uninstall | uninstalls OvercastC
+oc-hub version | prints the version of OvercastC and oc-hub installed
+oc-hub help | brings up this help page
+            ";
+            
+            if(args.Length == 0)
+            {
+                Console.WriteLine(helpText);
+            }
+            else
+            {
+                switch(args[0])
+                {
+                    case "install":
+                        new Installer().Install();
+                        break;
+                    case "uninstall":
+                        new Installer().Uninstall();
+                        break;
+                    case "version":
+                        Console.WriteLine("oc-hub: " + OC_HUB_VERSION);
+                        var basePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Overcast");
+                        if (Directory.Exists(basePath))
+                        {
+                            var ocVersion = File.ReadAllText(Path.Combine(basePath, "VERSION"));
+                            Console.WriteLine("overcast: " + ocVersion);
+                        }
+                        else
+                        {
+                            Console.WriteLine("No Overcast Installation Detected");
+                        }
+                        break;
+                    case "help":
+                        Console.WriteLine(helpText);
+                        break;
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
